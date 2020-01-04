634e388e4aeff57771c9ea03aaca7f0242d57037
Python Force <studio@pythonforce.com>
2020-01-04 16:45:46 -0500

    Update models.py
    on_delete=models.CASCADE - for Campaign

--- mailchimp/models.py
+++ mailchimp/models.py
@@ -173,7 +173,7 @@ class Campaign(models.Model):
     campaign_id = models.CharField(max_length=50)
     content = models.TextField()
     name = models.CharField(max_length=255)
-    content_type = models.ForeignKey(ContentType, null=True, blank=True)
+    content_type = models.ForeignKey(ContentType, null=True, blank=True, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField(null=True, blank=True)
     content_object = GenericForeignKey('content_type', 'object_id')
     extra_info = models.TextField(null=True)
